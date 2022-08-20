0a671d9b96abbfe1f7c13eb8d957b5d8d20a5b81
Sulaiman <Sulaiman@MacBook-Pro-de-MAIMAITITUERXUN.local>
2022-08-20 17:55:55 +0200

    first commit / create project

---
+++ AvisBackend/src/main/java/fr/orsys/ms/avisbackend/AvisBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.orsys.ms.avisbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvisBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvisBackendApplication.class, args);
+    }
+
+}
---
+++ AvisBackend/src/test/java/fr/orsys/ms/avisbackend/AvisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.orsys.ms.avisbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvisBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
